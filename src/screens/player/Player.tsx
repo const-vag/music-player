0bdfdbd5cc58d35d1d8c7f6500761c02a26f0cef
const vag <konsvag@gmail.com>
2023-05-26 23:33:11 +0300

    create dummy Player modal screen

--- src/screens/player/Player.tsx
+++ src/screens/player/Player.tsx
@@ -1,25 +1,50 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet } from "react-native";
-import { useTheme, Text } from "react-native-paper";
-import { Box } from "../../ui-kit/Box";
+import { IconButton, Text } from "react-native-paper";
+import { Box } from "../../ui-kit/Box/Box";
+import { Container } from "../../ui-kit/Container";
+import { Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export const Player = () => {
-  const theme = useTheme();
-
+  const navigation = useNavigation();
   return (
-    <Box style={styles.container}>
-      <Text>Player</Text>
-      <StatusBar style="auto" />
-    </Box>
+    <Container onClose={navigation.goBack}>
+      <Image
+        style={{
+          width: 250,
+          height: 250,
+        }}
+        source={{
+          uri: "https://storage.googleapis.com/pr-newsroom-wp/1/2022/02/SNIK1-1-1440x1440.png",
+        }}
+      />
+      <Box direction="row" mb={20}>
+        <IconButton
+          icon="skip-previous"
+          size={54}
+          onPress={() => console.log("Pressed")}
+        />
+        <Box mh={20}>
+          <IconButton
+            icon="play"
+            size={84}
+            onPress={() => console.log("Pressed")}
+          />
+        </Box>
+        <IconButton
+          icon="skip-next"
+          size={54}
+          onPress={() => console.log("Pressed")}
+        />
+      </Box>
+      <Box direction="row">
+        <Text variant="titleMedium">Snik, Trannos - GAMW TON LIGHT</Text>
+        <IconButton
+          icon="heart"
+          onPress={() => console.log("favourite song snikaros mono")}
+          size={34}
+        />
+      </Box>
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
