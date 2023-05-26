b26c2977b15ed3713d8e98914ed8bb227c6a74c7
const vag <konsvag@gmail.com>
2023-05-26 23:33:36 +0300

    add default values to MaterialIcon

--- src/ui-kit/MaterialIcon.tsx
+++ src/ui-kit/MaterialIcon.tsx
@@ -1,5 +1,16 @@
+import React, { ComponentProps } from "react";
+import { useTheme } from "react-native-paper";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 /**
  * @param name https://pictogrammers.com/library/mdi/
  */
-export { Icon as MaterialIcon };
+
+export type MaterialIconProps = ComponentProps<typeof Icon>;
+
+export const MaterialIcon = (props: MaterialIconProps) => {
+  const theme = useTheme();
+
+  const { color = theme.colors.onBackground, size = 24 } = props;
+
+  return <Icon {...props} color={color} size={size} />;
+};
