2a86553b6cb157993e7befa85cf355136123abd8
const vag <konsvag@gmail.com>
2023-06-06 20:06:49 +0300

    create form component

---
+++ src/form-components/TextField/TextField.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Controller, FieldValues } from "react-hook-form";
+import { TextInput, useTheme } from "react-native-paper";
+import { Box } from "../../ui-kit/Box/Box";
+import { Typography } from "../../ui-kit/Typography";
+import { TextFieldProps } from "./types";
+import { getMinWidth } from "./helpers";
+
+export const TextField = <TFormValues extends FieldValues>(
+  props: TextFieldProps<TFormValues>
+) => {
+  const theme = useTheme();
+  const {
+    control,
+    name,
+    rules,
+    sizeVariant = "half-size",
+    ...textInputProps
+  } = props;
+
+  return (
+    <Controller
+      name={name}
+      rules={rules}
+      control={control}
+      render={({
+        field: { onBlur, onChange, value },
+        fieldState: { error },
+      }) => (
+        <Box>
+          <TextInput
+            {...textInputProps}
+            style={[
+              { minWidth: getMinWidth(sizeVariant) },
+              textInputProps.style,
+            ]}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            error={Boolean(error)}
+          />
+          {error && (
+            <Typography
+              style={{ color: theme.colors.error }}
+              variant="bodySmall"
+            >
+              {error.message}
+            </Typography>
+          )}
+        </Box>
+      )}
+    />
+  );
+};
---
+++ src/form-components/TextField/helpers.ts
@@ -0,0 +1,7 @@
+import { TextFieldProps } from "./types";
+
+type SizeVariant = TextFieldProps["sizeVariant"];
+export const getMinWidth = (sizeVariant: SizeVariant) => {
+  if (sizeVariant === "half-size") return "50%";
+  return "100%";
+};
