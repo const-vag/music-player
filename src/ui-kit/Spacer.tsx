f6142e51a53fb0b726386fb52fbe0e30ab1e4b10
const vag <konsvag@gmail.com>
2023-06-06 20:08:27 +0300

    add ui kit components

---
+++ src/ui-kit/Spacer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box } from "./Box/Box";
+
+type SpacerProps = {
+  mode?: "vertical" | "horizontal";
+  size?: number;
+};
+export const Spacer = (props: SpacerProps) => {
+  const { mode = "vertical", size = 24 } = props;
+  return (
+    <Box
+      style={{
+        height: mode === "vertical" ? size : undefined,
+        width: mode === "horizontal" ? size : undefined,
+      }}
+    />
+  );
+};
---
+++ src/ui-kit/Typography.tsx
@@ -0,0 +1,2 @@
+import { Text } from "react-native-paper";
+export { Text as Typography };
