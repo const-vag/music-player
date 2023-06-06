ebccc465a5f0c13e97333d65ed36e827afb4f3b1
const vag <konsvag@gmail.com>
2023-06-06 20:09:02 +0300

    types for form component

---
+++ src/form-components/TextField/types.ts
@@ -0,0 +1,22 @@
+import { ComponentProps } from "react";
+import {
+  Control,
+  ControllerProps,
+  FieldPath,
+  FieldValues,
+} from "react-hook-form";
+import { TextInput } from "react-native-paper";
+
+type FormControllerProps<TFieldValues extends FieldValues, TContext = any> = {
+  control: Control<TFieldValues, TContext>;
+  name: FieldPath<TFieldValues>;
+  rules?: ControllerProps<TFieldValues>["rules"];
+};
+
+export type TextFieldProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TContext = any
+> = ComponentProps<typeof TextInput> &
+  FormControllerProps<TFieldValues, TContext> & {
+    sizeVariant?: "half-size" | "full-size";
+  };
