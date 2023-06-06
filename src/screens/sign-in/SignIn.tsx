9e950e72ddc8ab12942bb930db09b096c7ddb801
const vag <konsvag@gmail.com>
2023-06-06 20:09:29 +0300

    create sign in screen

--- src/screens/sign-in/SignIn.tsx
+++ src/screens/sign-in/SignIn.tsx
@@ -1,11 +1,38 @@
 import React from "react";
-import { Text } from "react-native";
 import { Container } from "../../ui-kit/Container";
+import { Typography } from "../../ui-kit/Typography";
+import { TextField } from "../../form-components/TextField/TextField";
+import { useSignIn } from "./useSignIn";
+import { Button } from "react-native-paper";
+import { Spacer } from "../../ui-kit/Spacer";
 
 export const SignIn = () => {
+  const { form, onSubmit } = useSignIn();
+
   return (
     <Container>
-      <Text>Sign in</Text>
+      <Typography variant="headlineMedium">Sign in</Typography>
+      <Spacer />
+      <TextField
+        rules={{
+          required: "Email is required",
+        }}
+        placeholder="Email"
+        name="email"
+        control={form.control}
+      />
+      <Spacer />
+      <TextField
+        rules={{ required: "Password is required" }}
+        placeholder="Password"
+        name="password"
+        secureTextEntry
+        control={form.control}
+      />
+      <Spacer />
+      <Button onPress={form.handleSubmit(onSubmit)} mode="contained">
+        Login
+      </Button>
     </Container>
   );
 };
