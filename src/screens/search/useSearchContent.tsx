b7ce39fdcef6d4775668b1afab14d8532c2ff38e
const vag <konsvag@gmail.com>
2023-07-08 18:06:16 +0300

    add comment for nesting navigation

--- src/screens/search/useSearchContent.tsx
+++ src/screens/search/useSearchContent.tsx
@@ -15,6 +15,10 @@ export const useSearchContent = () => {
   const navigation = useNavigation<StackNavigationProp<SearchStackParamList>>();
 
   const goToHawkTsibouki = () => {
+    /* 
+      Nesting navigation guide:
+      https://reactnavigation.org/docs/nesting-navigators#navigating-to-a-screen-in-a-nested-navigator
+    */
     navigation.navigate(SearchRoutes.CONTENT, {
       screen: ContentRoutes.ARTIST,
       params: {
