a897d5e10242b41c2614d620727a5882bd99562b
const vag <konsvag@gmail.com>
2023-07-16 18:25:40 +0300

    fix album and song card styling

--- src/screens/content/album/Album.tsx
+++ src/screens/content/album/Album.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import {
-  FlatList,
-  Image,
-  ListRenderItemInfo
-} from 'react-native';
+import { FlatList, Image, ListRenderItemInfo } from 'react-native';
 import { ActivityIndicator } from 'react-native-paper';
 import { Song } from '../../../api/requests/songs.api';
 import { Container } from '../../../ui-kit/Container';
 import { Typography } from '../../../ui-kit/Typography';
 import { SongCard } from './components/SongCard';
 import { useAlbum } from './useAlbum';
+import { Spacer } from '../../../ui-kit/Spacer';
+import { Box } from '../../../ui-kit/Box/Box';
 
 const IMAGE_SIZE = 200;
 
@@ -27,16 +25,25 @@ export const Album = () => {
 
   return (
     <Container>
-      <Image
-        style={{ width: IMAGE_SIZE, height: IMAGE_SIZE }}
-        source={{ uri: album.image }}
+      <Box>
+        <Image
+          style={{ width: IMAGE_SIZE, height: IMAGE_SIZE }}
+          source={{ uri: album.image }}
+        />
+        <Typography variant="titleLarge">{album.name}</Typography>
+        <Spacer />
+      </Box>
+      <FlatList
+        removeClippedSubviews
+        ItemSeparatorComponent={() => <Spacer size={10} />}
+        data={album.songs}
+        keyExtractor={keyExtractor}
+        renderItem={({ item }: ListRenderItemInfo<Song>) => (
+          <SongCard song={item} />
+        )}
       />
-      <Typography>{album.name}</Typography>
-      <FlatList data={album.songs} renderItem={renderItem} />
     </Container>
   );
 };
 
-const renderItem = ({ item }: ListRenderItemInfo<Song>) => (
-  <SongCard song={item} />
-);
+const keyExtractor = (song: Song) => `${song.id}`;
--- src/screens/content/album/components/SongCard.tsx
+++ src/screens/content/album/components/SongCard.tsx
@@ -2,19 +2,38 @@ import { memo } from 'react';
 import { Song } from '../../../../api/requests/songs.api';
 import { Box } from '../../../../ui-kit/Box/Box';
 import { Typography } from '../../../../ui-kit/Typography';
+import { TouchableOpacity } from 'react-native';
+import { IconButton } from 'react-native-paper';
+import { useSongCard } from './useSongCard';
 
 type SongCardProps = {
   song: Song;
 };
 
 const SongCardComponent = ({ song }: SongCardProps) => {
+  const { likeSongMutation } = useSongCard();
   return (
-    <Box ml={0} style={{ alignItems: 'flex-start' }}>
-      <Typography variant="titleMedium">{song.name}</Typography>
-      <Typography variant="bodySmall">
-        {song.artists.map((artist) => artist.name).join(', ')}
-      </Typography>
-    </Box>
+    <TouchableOpacity onPress={() => console.log('Play song...')}>
+      <Box expand>
+        <Box
+          style={{
+            justifyContent: 'space-between',
+          }}
+          direction="row"
+        >
+          <Box style={{ alignItems: 'flex-start' }}>
+            <Typography variant="titleMedium">{song.name}</Typography>
+            <Typography variant="bodySmall">
+              {song.artists.map((artist) => artist.name).join(', ')}
+            </Typography>
+          </Box>
+          <IconButton
+            icon="heart"
+            onPress={() => likeSongMutation.mutate(song.id)}
+          />
+        </Box>
+      </Box>
+    </TouchableOpacity>
   );
 };
 
