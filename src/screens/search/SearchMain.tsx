ec363a97d983dad20d9e4ec47b81d0852caeafab
const vag <konsvag@gmail.com>
2023-07-16 18:26:35 +0300

    add spacer to search main screen

--- src/screens/search/SearchMain.tsx
+++ src/screens/search/SearchMain.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Button } from 'react-native-paper';
 import { Container } from '../../ui-kit/Container';
 import { useSearchMain } from './useSearchMain';
+import { Spacer } from '../../ui-kit/Spacer';
 
 export const SearchMain = () => {
   const { goToHawkTsibouki } = useSearchMain();
   return (
     <Container>
+      <Spacer />
       <Button mode="contained" onPress={goToHawkTsibouki}>
         Go to hawk
       </Button>
