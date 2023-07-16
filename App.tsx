d6bd34bb28d91e87568b57d3b89842a11cb48198
const vag <konsvag@gmail.com>
2023-07-16 13:54:24 +0300

    change app to dark theme

--- App.tsx
+++ App.tsx
@@ -4,11 +4,9 @@ import React from 'react';
 import {
   PaperProvider,
   MD3DarkTheme,
-  MD3LightTheme,
   MD3Theme,
 } from 'react-native-paper';
 import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
-import { useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppNavigator } from './src/navigator/AppNavigator';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -16,13 +14,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 export default function App() {
-  const colorScheme = useColorScheme();
   const { theme } = useMaterial3Theme();
 
-  const paperTheme: MD3Theme =
-    colorScheme === 'dark'
-      ? { ...MD3DarkTheme, colors: theme.dark, mode: 'adaptive' }
-      : { ...MD3LightTheme, colors: theme.light, mode: 'adaptive' };
+  const paperTheme: MD3Theme = { ...MD3DarkTheme, colors: theme.dark }
 
   return (
     <QueryClientProvider client={queryClient}>
